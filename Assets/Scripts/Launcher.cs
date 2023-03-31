74b0d563388fff8bdce82ffa8e0d820f79d5b681
H4nzo <mtminimah@griotstudios.org>
2023-03-31 13:43:10 +0100

    Added Launcher Script

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         Debug.Log("Connected to Photon Network");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Joined Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+
+        Debug.Log("Joined Lobby");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
