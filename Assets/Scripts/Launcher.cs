ae55332630558ce09b9e7144b15ac84f06c1a786
H4nzo <mtminimah@griotstudios.org>
2023-04-01 08:13:24 +0100

    Find Room System

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
 
 namespace Hanzo
 {
     public class Launcher : MonoBehaviourPunCallbacks
     {
+        public static Launcher Instance;
+
         [SerializeField] TMP_InputField roomNameInputfield;
         [SerializeField] TMP_Text errorText;
         [SerializeField] TMP_Text roomNameText;
+
+        public Transform roomListBG;
+        public GameObject roomListPrefab;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
 
         // Start is called before the first frame update
         void Start()
@@ -52,12 +63,26 @@ namespace Hanzo
 
         public void LeaveRoom()
         {
-                PhotonNetwork.LeaveRoom();
+            PhotonNetwork.LeaveRoom();
         }
 
         public override void OnLeftRoom()
         {
-           MenuManager.Instance.OpenMenu("title");
+            MenuManager.Instance.OpenMenu("title");
+        }
+
+        public void JoinRoom(RoomInfo info)
+        {
+            PhotonNetwork.JoinRoom(info.Name);
+            MenuManager.Instance.OpenMenu("loading");
+        }
+
+        public override void OnRoomListUpdate(List<RoomInfo> roomList)
+        {
+            for (int i = 0; i < roomList.Count; i++)
+            {
+                Instantiate(roomListPrefab, roomListBG).GetComponent<RoomListItem>().Setup(roomList[i]);
+            }
         }
 
 
